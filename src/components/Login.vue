04aec5c8a703543cc7945d357e5fe75dc772ce7a
mijailCR12 <66917852+mijailCR12@users.noreply.github.com>
2023-10-30 22:04:56 -0600

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="login-container">
+      <h2>Iniciar sesión</h2>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="email">Correo electrónico:</label>
+          <input type="email" id="email" v-model="email" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Contraseña:</label>
+          <input type="password" id="password" v-model="password" required>
+        </div>
+        <button type="submit">Iniciar sesión</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: "",
+        password: "",
+      };
+    },
+    methods: {
+      login() {
+        // Aquí puedes agregar la lógica de autenticación
+        // Verificar el correo y contraseña, y realizar la redirección
+        if (this.email === "usuario@example.com" && this.password === "contraseña") {
+          // Redirige al usuario a la página "Home" después de iniciar sesión exitosamente
+          this.$router.push("/home");
+        } else {
+          // En caso de credenciales incorrectas, muestra un mensaje de error o toma otra acción
+          console.error("Credenciales incorrectas");
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .login-container {
+    max-width: 300px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  .form-group {
+    margin-bottom: 10px;
+  }
+  
+  label {
+    font-weight: bold;
+  }
+  
+  input {
+    width: 100%;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+  }
+  
+  button {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+  }
+  </style>
+  
