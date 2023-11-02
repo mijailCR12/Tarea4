7fd109b5a905215b05d51c8523e56f7ea0cc387e
mijailCR12 <66917852+mijailCR12@users.noreply.github.com>
2023-11-01 21:35:34 -0600

    login 2

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,91 +1,91 @@
-"use strict"
-
-const headers = require('./headersCORS');
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-
-exports.handler = async (event, context) => {
-
-  if (event.httpMethod == "OPTIONS") {
-    return {statusCode: 200,headers,body: "OK"};
-  }
-
-  try {
-
-    const data = JSON.parse(event.body);
-
-    if (!(data.email && data.password)) {
-      return { statusCode: 400, headers, body:"All input is required"};
-    }
-
-    let user = {'_id':1,'token':''};
-    let passwd = await bcrypt.hash('12345', 10);
-
-    if (await bcrypt.compare(data.password, passwd)) {
-      user.token = jwt.sign(
-         { user_id: user._id, email:data.email },
-         process.env.TOKEN_KEY,
-         {
-           expiresIn: "2h",
-         }
-       );
-      return { statusCode: 200, headers, body: JSON.stringify(user)};
-    }
-		return { statusCode: 400, headers, body: 'Invalid Credentials' };
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 422, headers, body: JSON.stringify(error) };
-  }
-};
-
-
-
 // "use strict"
 
 // const headers = require('./headersCORS');
 // const jwt = require("jsonwebtoken");
 // const bcrypt = require("bcryptjs");
-// const { MongoClient } = require("mongodb");
 
 // exports.handler = async (event, context) => {
 
 //   if (event.httpMethod == "OPTIONS") {
-//     return { statusCode: 200, headers, body: "OK" };
+//     return {statusCode: 200,headers,body: "OK"};
 //   }
 
 //   try {
+
 //     const data = JSON.parse(event.body);
 
 //     if (!(data.email && data.password)) {
-//       return { statusCode: 400, headers, body: "All input is required" };
+//       return { statusCode: 400, headers, body:"All input is required"};
 //     }
 
-//     // Configura la conexión a MongoDB
-//     const uri = "mongodb://usuario:contraseña@host:puerto/base_de_datos";
-//     const client = new MongoClient(uri, { useNewUrlParser: true });
+//     let user = {'_id':1,'token':''};
+//     let passwd = await bcrypt.hash('12345', 10);
 
-//     try {
-//       await client.connect();
-//       const user = await client.db("tarea").collection("usuarios").findOne({ email: data.email });
-
-//       if (user && (await bcrypt.compare(data.password, user.password))) {
-//         user.token = jwt.sign(
-//           { user_id: user._id, email: data.email },
-//           process.env.TOKEN_KEY,
-//           {
-//             expiresIn: "2h",
-//           }
-//         );
-//         return { statusCode: 200, headers, body: JSON.stringify(user)};
-//       } else {
-//         return { statusCode: 400, headers, body: 'Invalid Credentials' };
-//       }
-//     } finally {
-//       // Cierra la conexión con MongoDB
-//       await client.close();
+//     if (await bcrypt.compare(data.password, passwd)) {
+//       user.token = jwt.sign(
+//          { user_id: user._id, email:data.email },
+//          process.env.TOKEN_KEY,
+//          {
+//            expiresIn: "2h",
+//          }
+//        );
+//       return { statusCode: 200, headers, body: JSON.stringify(user)};
 //     }
+// 		return { statusCode: 400, headers, body: 'Invalid Credentials' };
 //   } catch (error) {
 //     console.log(error);
 //     return { statusCode: 422, headers, body: JSON.stringify(error) };
 //   }
 // };
+
+
+
+"use strict"
+
+const headers = require('./headersCORS');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { MongoClient } = require("mongodb");
+
+exports.handler = async (event, context) => {
+
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+
+  try {
+    const data = JSON.parse(event.body);
+
+    if (!(data.email && data.password)) {
+      return { statusCode: 400, headers, body: "All input is required" };
+    }
+
+    // Configura la conexión a MongoDB
+    const uri = "mongodb://usuario:contraseña@host:puerto/base_de_datos";
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+
+    try {
+      await client.connect();
+      const user = await client.db("tarea").collection("usuarios").findOne({ email: data.email });
+
+      if (user && (await bcrypt.compare(data.password, user.password))) {
+        user.token = jwt.sign(
+          { user_id: user._id, email: data.email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+        return { statusCode: 200, headers, body: JSON.stringify(user)};
+      } else {
+        return { statusCode: 400, headers, body: 'Invalid Credentials' };
+      }
+    } finally {
+      // Cierra la conexión con MongoDB
+      await client.close();
+    }
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 422, headers, body: JSON.stringify(error) };
+  }
+};
