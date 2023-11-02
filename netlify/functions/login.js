19da786023aa4a67d67edb5a2cb9a71dd2c04509
mijailCR12 <66917852+mijailCR12@users.noreply.github.com>
2023-11-01 22:01:51 -0600

    login5

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -78,7 +78,9 @@ exports.handler = async (event, context) => {
         );
         return { statusCode: 200, headers, body: JSON.stringify(user)};
       } else {
-        return { statusCode: 400, headers, body: 'Invalid Credentials', body2: JSON.stringify(user) };
+        // return { statusCode: 400, headers, body: 'Invalid Credentials', body2: JSON.stringify(user) };
+        
+        return { statusCode: 400, headers, body: JSON.stringify(user) };
       }
     } finally {
       // Cierra la conexión con MongoDB
